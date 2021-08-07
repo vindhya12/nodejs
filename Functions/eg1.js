b923b2ccea3d906c1da8c47d0c7c2d51e10123e7
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-08-07 16:08:30 +0530

    Create eg1.js

---
+++ Functions/eg1.js
@@ -0,0 +1,16 @@
+console.log(10);
+var x;
+x=10;
+console.log(x);
+x="HEllo vindhya welcome to node js ";
+console.log(x);
+x='hello vindhya welcome ';
+console.log(x);
+
+x=true;
+console.log(x);
+x=23.78;
+console.log(x);
+var y;
+console.log(y);
+console.log(z);
